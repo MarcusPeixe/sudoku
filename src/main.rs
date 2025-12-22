6bb3bce7ee2df26de3200dcc96f755b4f429dc6a
MarcusPeixe <marcusv.peixoto23@gmail.com>
2025-12-22 18:09:32 -0300

    Initial commit II

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
 use crossterm::{
-    event::{self, Event, KeyEvent},
-    execute,
-    terminal::{self, disable_raw_mode, enable_raw_mode},
+    cursor, event::{self, Event, KeyEvent}, execute, terminal::{self, disable_raw_mode, enable_raw_mode}
 };
 use std::io;
 
+struct Number(u8);
+
+impl Number {
+    fn new(value: u8) -> Option<Self> {
+        (1..=9).contains(&value).then_some(Self(value))
+    }
+}
+
+enum Cell {
+    Empty { pencil_mask: u16 },
+    Filled { value: u8 },
+}
+
 fn main() -> io::Result<()> {
+    let (w, h) = terminal::size()?;
+    if w < 27 || h < 27 {
+        eprintln!("Terminal size must be at least 27x27");
+        return Ok(());
+    }
+
     enable_raw_mode()?;
-    
-    execute!(io::stdout(), terminal::ScrollUp(10))?;
 
     // loop {
     //     let event = event::read()?;
@@ -23,6 +38,5 @@ fn main() -> io::Result<()> {
     // }
     
     disable_raw_mode()?;
-    execute!(io::stdout(), terminal::LeaveAlternateScreen)?;
     Ok(())
 }
